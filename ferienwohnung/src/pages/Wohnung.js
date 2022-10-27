c11cc3daadbf8108456424fc304f880a2a42c9e8
Thyriion <kevinschueler2000@gmail.com>
2022-10-27 18:44:32 +0200

    #1 Starting fresh on a new project :)

--- ferienwohnung/src/pages/Wohnung.js
+++
@@ -1,5 +0,0 @@
-function Wohnung() {
-    return <h1>Wohnung</h1>;
-}
-
-export default Wohnung;
--- ferienwohnung/src/pages/Wohnung2.js
+++
@@ -1,5 +0,0 @@
-function Wohnung2() {
-    return <h1>Wohnung 2</h1>;
-}
-
-export default Wohnung2;
