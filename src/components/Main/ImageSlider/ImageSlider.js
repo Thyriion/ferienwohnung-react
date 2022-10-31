3167a5cffc78f9a5438b036e638f1d3e3b741cae
Thyriion <kevinschueler2000@gmail.com>
2022-10-31 17:50:55 +0100

    Image Slider

--- src/components/Main/ImageSlider/ImageSlider.js
+++ src/components/Main/ImageSlider/ImageSlider.js
@@ -1,51 +1,22 @@
-import React, { useState } from 'react';
-import './ImageGallery.scss';
+import React, { useEffect, useState, useCallback } from 'react';
+import './ImageSliderStyles.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faCircleChevronLeft,
     faCircleChevronRight,
     faCircleXmark,
 } from '@fortawesome/free-solid-svg-icons';
+import { ImageData } from './ImageSliderData';
 
 export default function ImageGallery() {
-    const ImageGallery = [
-        {
-            img: 'https://images.unsplash.com/photo-1666468426668-917784a15335?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1666468426668-917784a15335?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80https://images.unsplash.com/photo-1666834744271-af5ef132a179?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1666639577881-d04201a4545a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1666668321985-105042873ddc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1656836678116-c6766b8b3d8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1666844550308-9b47df48af49?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1666837147745-1c9dea9908a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1666625628272-a1071f6f7173?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1651525763614-bbdf80ba2a35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1652939056174-615553a7bf76?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        },
-    ];
+    const ImageGallery = ImageData;
 
     const [slideNumber, setSlideNumber] = useState(0);
     const [openModal, setOpenModal] = useState(false);
+    const [autoPlay, setAutoPlay] = useState(true);
+    const [timeOut, setSliderTimeout] = useState(2500);
 
-    const handleOpenModal = (index) => {
-        setSlideNumber(index);
+    const handleOpenModal = () => {
         setOpenModal(true);
     };
 
@@ -59,11 +30,30 @@ export default function ImageGallery() {
             : setSlideNumber(slideNumber - 1);
     };
 
-    const nextSlide = () => {
+    const nextSlide = useCallback(() => {
         slideNumber + 1 === ImageGallery.length
             ? setSlideNumber(0)
             : setSlideNumber(slideNumber + 1);
+    }, [slideNumber, ImageGallery.length]);
+
+    const createTimeOut = () => {
+        autoPlay &&
+            setSliderTimeout(
+                setTimeout(() => {
+                    nextSlide();
+                }, 2500)
+            );
     };
+
+    useEffect(createTimeOut, [slideNumber, autoPlay, nextSlide]);
+    useEffect(() => {
+        if (openModal) {
+            setAutoPlay(false);
+            clearTimeout(timeOut);
+        } else {
+            setAutoPlay(true);
+        }
+    }, [setAutoPlay, openModal, timeOut]);
 
     return (
         <>
@@ -94,7 +84,16 @@ export default function ImageGallery() {
                 </div>
             )}
 
-            <div className={openModal ? 'galleryWrap hidden' : 'galleryWrap'}>
+            <div
+                className='galleryWrap'
+                onMouseEnter={() => {
+                    setAutoPlay(false);
+                    clearTimeout(timeOut);
+                }}
+                onMouseLeave={() => {
+                    clearTimeout(timeOut);
+                    setAutoPlay(true);
+                }}>
                 <div className="btnPrevContainer" onClick={prevSlide}>
                     <FontAwesomeIcon
                         icon={faCircleChevronLeft}
@@ -113,7 +112,11 @@ export default function ImageGallery() {
                             <div
                                 className="single"
                                 key={index}
-                                onClick={() => handleOpenModal(index)}>
+                                onClick={() => {
+                                    handleOpenModal(index);
+                                    setAutoPlay(false);
+                                    clearTimeout(timeOut);
+                                }}>
                                 <img
                                     src={slide.img}
                                     alt=""
